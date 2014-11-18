6a00cf0bdbc5b6bfb0b05acb809c10fa16c60002
Fabian Schultz <fschultz02@gmail.com>
2014-11-18 18:28:38 +0100

    Updated json url

--- a-w/js/sketch.js
+++ a-w/js/sketch.js
@@ -50,7 +50,7 @@ function kimonoCallback(data) {
 };
 
 $.ajax({
-    "url": "http://0.0.0.0:8000/p5/data/weather-" + cityCode + ".json",
+    "url": "http://fabe.github.io/a-w/data/weather-" + cityCode + ".json",
         "crossDomain": false,
         "dataType": "jsonp"
 });

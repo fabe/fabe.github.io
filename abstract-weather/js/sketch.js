9e0bf61d5a75658b5fa52b1eaa133e76f0bc238e
Fabian Schultz <fschultz02@gmail.com>
2015-03-28 23:06:38 +0100

    Change URL of data

--- abstract-weather/js/sketch.js
+++ abstract-weather/js/sketch.js
@@ -33,7 +33,7 @@ function preload() {
     mymap = loadImage("img/world.png");
     
     $.ajax({
-    "url": "//fabe.github.io/a-w/data/weather-" + cityCode + ".json?callback=kimonoCallback",
+    "url": "//fabe.github.io/abstract-weather/data/weather-" + cityCode + ".json?callback=kimonoCallback",
         "crossDomain": false,
         "dataType": "jsonp"
     });

6a763c8b5940729fbef2bc75651c77684b4b2156
Fabian Schultz <fschultz02@gmail.com>
2015-04-08 17:55:12 +0200

    Update sketch.js

--- abstract-weather/js/sketch.js
+++ abstract-weather/js/sketch.js
@@ -38,107 +38,92 @@ $.ajax({
         "crossDomain": false,
         "dataType": "jsonp"
 }).done(function () {
-    function kimonoCallback(data) {
-        console.log(data);
-        var w = data.results.collection1;
-        for (var i = 0; i < w.length; i++) {
-            var celsius = Math.round(w[i].avg);
-            counter.push(celsius);
-        }
-        largest = Math.max.apply(Math, counter);
-        lowest = Math.min.apply(Math, counter);
-
-        for (var k = 0; k < counter.length; k++) {
-            sum += parseInt(counter[k], 10);
-        }
-        avg = sum / counter.length;
-
-        city = data.city;
-
-        lat = data.lat;
-        lon = data.lon;
+    console.log(data);
+    var w = data.results.collection1;
+    for (var i = 0; i < w.length; i++) {
+        var celsius = Math.round(w[i].avg);
+        counter.push(celsius);
     }
+    largest = Math.max.apply(Math, counter);
+    lowest = Math.min.apply(Math, counter);
 
-    function setup() {
-        createCanvas(mymap.width, mymap.height - square);
-        x = map(lon, -180, 180, 0, width);
-        y = map(lat, 90, -90, 0, height);
+    for (var k = 0; k < counter.length; k++) {
+        sum += parseInt(counter[k], 10);
+    }
+    avg = sum / counter.length;
 
-        // image(mymap, 0, 0);
+    city = data.city;
 
-        var colorIndicator = random(100);
+    lat = data.lat;
+    lon = data.lon;
+});
+
+function setup() {
+    createCanvas(mymap.width, mymap.height - square);
+    x = map(lon, -180, 180, 0, width);
+    y = map(lat, 90, -90, 0, height);
+
+    // image(mymap, 0, 0);
+
+    var colorIndicator = random(100);
 
 
 
-        if (differentColors === true) {
-            if (colorIndicator < 25) {
-                c1 = color(50, 165, 160);
-                c2 = color(217, 235, 161);
-                c3 = color(12, 71, 79);
-            } else if (colorIndicator < 50 && colorIndicator > 25) {
-                c1 = color(103, 42, 187);
-                c2 = color(146, 249, 227);
-                c3 = color(35, 20, 55);
-            } else if (colorIndicator < 75 && colorIndicator > 50) {
-                c1 = color(252, 61, 32);
-                c2 = color(252, 215, 4);
-                c3 = color(38, 33, 48);
-            } else {
-                c1 = color(249, 101, 63);
-                c2 = color(249, 218, 146);
-                c3 = color(35, 35, 35);
-            }
-        } else {
+    if (differentColors === true) {
+        if (colorIndicator < 25) {
             c1 = color(50, 165, 160);
             c2 = color(217, 235, 161);
             c3 = color(12, 71, 79);
+        } else if (colorIndicator < 50 && colorIndicator > 25) {
+            c1 = color(103, 42, 187);
+            c2 = color(146, 249, 227);
+            c3 = color(35, 20, 55);
+        } else if (colorIndicator < 75 && colorIndicator > 50) {
+            c1 = color(252, 61, 32);
+            c2 = color(252, 215, 4);
+            c3 = color(38, 33, 48);
+        } else {
+            c1 = color(249, 101, 63);
+            c2 = color(249, 218, 146);
+            c3 = color(35, 35, 35);
         }
-
-
-        $("canvas").css({
-            border: "25px solid " + c3.colorString
-        });
-        $("#city").text(String(city));
-        $("#city").css({
-            color: c3.colorString
-        });
+    } else {
+        c1 = color(50, 165, 160);
+        c2 = color(217, 235, 161);
+        c3 = color(12, 71, 79);
     }
 
-    function draw() {
-        for (var j = 0; j <= 364; j++) {
-            var lerp = map(counter[j], lowest, largest, 0, 1);
 
-            var c = lerpColor(c1, c2, lerp);
+    $("canvas").css({
+        border: "25px solid " + c3.colorString
+    });
+    $("#city").text(String(city));
+    $("#city").css({
+        color: c3.colorString
+    });
 
-            if (rectX >= mymap.width - square * 2) {
-                rectY = rectY + square;
-                rectX = 0;
-            } else {
-                rectX = rectX + square;
-            }
-            fill(c);
-            noStroke();
-            rect(rectX, rectY, square, square);
+    for (var j = 0; j <= 364; j++) {
+        var lerp = map(counter[j], lowest, largest, 0, 1);
 
-            // text(String(j),rectX,rectY,square,square);
+        var c = lerpColor(c1, c2, lerp);
+
+        if (rectX >= mymap.width - square * 2) {
+            rectY = rectY + square;
+            rectX = 0;
+        } else {
+            rectX = rectX + square;
         }
+        fill(c);
+        noStroke();
+        rect(rectX, rectY, square, square);
 
-        fill(0);
-        stroke(c3);
-        strokeWeight(25);
-
-        line(0, y, width, y);
-        line(x, 0, x, width);
-
-    /* 
-    city = city.replace(/[aeiou]/ig,'').toUpperCase();
-    textSize(200);
-    textFont("Apercu, Helvetica");
-    textStyle(BOLD);
-    textAlign(CENTER);
-    noStroke();
-    fill(26, 25, 25);
-    text(city, 0, 0, width, height);
-    */
+        // text(String(j),rectX,rectY,square,square);
     }
-});
+
+    fill(0);
+    stroke(c3);
+    strokeWeight(25);
+
+    line(0, y, width, y);
+    line(x, 0, x, width);
+}
